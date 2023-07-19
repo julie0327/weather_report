7cb395a466ff36cce478a8cf0f0aed3bad945db1
Jully Ye <jye@advin.local>
2023-07-19 16:10:43 -0700

    add button function

---
+++ script.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector(".go");
+let right = document.querySelector(".right");
+let weather_display = document.querySelector(".weather_display");
+btn.addEventListener("click", function () {
+  //   right.appendChild(weather_display);
+  weather_display.style.weather_display = "flex";
+  console.log("!!!!");
+});
